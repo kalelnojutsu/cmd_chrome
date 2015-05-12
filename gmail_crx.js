045fd1818fca0318af0d46fa590c2a72e306ba08
kalelnojutsu <kalelnojutsu@gmail.com>
2015-05-12 15:03:27 +0200

    append after show

--- gmail_crx.js
+++ gmail_crx.js
@@ -72,12 +72,12 @@ dropdownContent.css("width", "275px"), dropdownContent.css("max-width", "275px")
     onClick: function(event) {
       //cmd.inboxSDK.composeView = sdk;
       var n = $(event.dropdown.el);
-      n.append(dropdownContent);
+      dropdownContent.show(), n.append(dropdownContent);
       
       function handleFileSelect(evt) {
         var files = evt.target.files; // FileList object
         for (var i = 0, f; f = files[i]; i++) {
-          dropdownContent.show();
+          
           cmd.showLoadingView();
           var reader = new FileReader();
           reader.file = f;
