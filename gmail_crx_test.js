627e504a5d6061ab1c31716046bed8fb3eebb6b1
kalelnojutsu <kalelnojutsu@gmail.com>
2015-06-08 10:58:52 +0200

    add depot test to url

--- gmail_crx_test.js
+++ gmail_crx_test.js
@@ -16,7 +16,7 @@
         chrome.runtime.sendMessage({
             method: 'POST',
             action: 'xhttp',
-            url: 'http://app.close-more.deals/connect'
+            url: 'http://'+depot.app+'.close-more.deals/connect'
         }, function(responseText) {
             $("#loading-view").hide();
             $("#doc-load-title").hide();
@@ -38,7 +38,7 @@
                 $('#doc-title').hide();
                 $('#info-recipients').show();
                 $( "#insert" ).attr('nid',$(this).attr('nid')).show();
-                var cover_link = "http://app.close-more.deals/cover/120/140/o/c/"+$(this).attr('nid')+".gif";
+                var cover_link = "http://"+depot.app+".close-more.deals/cover/120/140/o/c/"+$(this).attr('nid')+".gif";
                 $("#cover").append("<img id=\"vignette\" src=\""+cover_link+"\">").show();
                 $('.doc').hide();
                 $(this).attr('sel','yes').show();
@@ -74,6 +74,8 @@
     } 
   }
 
+depot={'app':'test'};
+
 var dropdownContent = $("#cmd-popup-layout");
 dropdownContent.css("width", "275px"), dropdownContent.css("max-width", "275px"), dropdownContent.css("padding","10px 5px 25px 5px"), InboxSDK.load(1, 'sdk_cmdgmailcrx_09bb9d5929').then(function(sdk){
   cmd.inboxSDK = sdk, cmd.inboxSDK.Compose.registerComposeViewHandler(function(sdk){
@@ -96,7 +98,7 @@ dropdownContent.css("width", "275px"), dropdownContent.css("max-width", "275px")
         chrome.runtime.sendMessage({
             method: 'POST',
             action: 'xhttp',
-            url: 'http://app.close-more.deals/connect'
+            url: 'http://'+depot.app+'.close-more.deals/connect'
         }, function(responseText) {
             var response = JSON.parse(responseText);
             var uid = response['user'][0]['uid'];
@@ -143,7 +145,7 @@ dropdownContent.css("width", "275px"), dropdownContent.css("max-width", "275px")
               method: 'POST',
               action: 'xhttp',
               data: rawData,
-              url: 'http://app.close-more.deals/add_file_gmail'
+              url: 'http://'+depot.app+'.close-more.deals/add_file_gmail'
             }, function(responseText) {
               responseText = JSON.parse(responseText);
               var thumbUrl = "https://d2qvtfnm75xrxf.cloudfront.net/public/extension/adobePdfIcon.png";
