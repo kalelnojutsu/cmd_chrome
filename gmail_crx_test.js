fe3128a67a58c1412bd1fa23df3e9b189ed70399
kalelnojutsu <kalelnojutsu@gmail.com>
2015-06-23 16:53:58 +0200

    new data to send

--- gmail_crx_test.js
+++ gmail_crx_test.js
@@ -218,18 +218,13 @@ dropdownContent.css("width", "275px"), dropdownContent.css("max-width", "275px")
     //composeView.insertTextIntoBodyAtCursor('Wazaaa');
     var re = /http:\/\/l.booklet.io\/zh5\/(\d+)$/;  
     var t = sdk.getToRecipients();
+    var c = sdk.getFromContact();
+    var subject = sdk.getSubject()
     var b = $(sdk.getBodyElement());
     var a = b.find('a');
-    chrome.runtime.sendMessage({
-            method: 'POST',
-            action: 'xhttp',
-            url: 'http://app.close-more.deals/connect'
-        }, function(responseText) {
-        	var data_track = '{ "email": "'+t[0].emailAddress+'" }';
-    		var url_tracking = 'http://test.close-more.deals/pixelbob.gif.php?d='+Date.now()+'&r='+btoa(responseText);
-    		sdk.insertHTMLIntoBodyAtCursor('<img width="1px" height="1px" src="'+encodeURI(url_tracking)+'">');
-    });
-
+    var data_track = '{"messageID":"'+sdk.getMessageID()+'", "subject":"'+subject+'", "from": "'+c[0]+'", "email": "'+t[0].emailAddress+'" }';
+    var url_tracking = 'http://test.close-more.deals/pixelbob.gif.php?d='+Date.now()+'&r='+btoa(data_track);
+    sdk.insertHTMLIntoBodyAtCursor('<img width="1px" height="1px" src="'+encodeURI(url_tracking)+'">');
 
     a.each(function( index ) {
       if(re.test($(this).attr('href')) && t.length==1){
