1ca595a61b11c7e2aad9a805b80fb9452dba9115
kalelnojutsu <kalelnojutsu@gmail.com>
2015-06-25 15:06:46 +0200

    track onsending

--- gmail_crx_test.js
+++ gmail_crx_test.js
@@ -248,7 +248,11 @@ dropdownContent.css("width", "275px"), dropdownContent.css("max-width", "275px")
     var a = b.find('a');
     var data_track = '{"messageID":"'+sdk.getMessageID()+'", "subject":"'+subject+'", "body": "'+sdk.getTextContent().replace(/"/g, '\\"').replace(/'/g, "\\'")+'", "from": "'+cmd.inboxSDK.User.getEmailAddress()+'", "email": "'+t[0].emailAddress+'" }';
     var url_tracking = 'http://test.close-more.deals/pixelbob.gif.php?d='+Date.now()+'&r='+btoa(data_track);
-    sdk.insertHTMLIntoBodyAtCursor('<img width="1px" height="1px" src="'+encodeURI(url_tracking)+'">');
+    if($('img[track]')){
+    	if($('img[track=yes]'))
+   		    sdk.insertTextIntoBodyAtCursor('Track! '+ data_track);
+   		    //sdk.insertHTMLIntoBodyAtCursor('<img width="1px" height="1px" src="'+encodeURI(url_tracking)+'">');
+    }
 
     a.each(function( index ) {
       if(re.test($(this).attr('href')) && t.length==1){
