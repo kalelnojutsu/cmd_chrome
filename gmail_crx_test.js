ad6deee0f0448103b8af929525bb9dffc98ff05c
kalelnojutsu <kalelnojutsu@gmail.com>
2015-06-26 12:48:09 +0200

    insert frame

--- gmail_crx_test.js
+++ gmail_crx_test.js
@@ -110,7 +110,17 @@ dropdownContent.css("width", "275px"), dropdownContent.css("max-width", "275px")
 
 	sdk.Router.handleCustomRoute(routeID, function(customRouteView){
 		//customRouteView.getElement().textContent = 'Vous avez ' + customRouteView.getParams().leads+' leads!';
-		customRouteView.getElement().HTMLContent = '<iframe src="http://app.close-more.deals/home" width="100%"></iframe>';
+		var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+		if (!location.ancestorOrigins.contains(extensionOrigin)) {
+		    var iframe = document.createElement('iframe');
+		    // Must be declared at web_accessible_resources in manifest.json
+		    iframe.src = chrome.runtime.getURL('frame.html');
+		    // Some styles for a fancy sidebar
+		    iframe.style.cssText = 'position:fixed;top:0;left:0;display:block;' +
+		                           'width:300px;height:100%;z-index:1000;';
+		    document.body.appendChild(iframe);
+		}
+		//customRouteView.getElement().HTMLContent = '<iframe src="http://app.close-more.deals/home" width="100%"></iframe>';
 	});
 
 	sdk.NavMenu.addNavItem({
